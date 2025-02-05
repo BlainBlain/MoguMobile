8c673575e9338cef75550609c609ec001c6b702b
OtakuMuch <nanimo1223@protonmail.com>
2025-02-05 20:35:05 +0800

    Enhance RestaurantInfoCard with rating display, open status indicator, and address styling

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,6 +1,11 @@
 import React from "react";
 import styled from "styled-components/native";
 import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
+import { Spacer } from "../../../components/spacer/spacer.component";
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -14,27 +19,91 @@ const RestaurantCardCover = styled(Card.Cover)`
 const Title = styled.Text`
   font-family: ${(props) => props.theme.fonts.heading};
   font-size: ${(props) => props.theme.fontSizes.title};
-  padding: ${(props) => props.theme.space[3]};
   color: ${(props) => props.theme.colors.text.primary};
+  padding-left: ${(props) => props.theme.space[1]};
+`;
+
+const Address = styled.Text`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+  color: ${(props) => props.theme.colors.text.secondary};
+  padding-left: ${(props) => props.theme.space[1]};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Section = styled.View`
+  flex-direction: row;
+`;
+
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+const Unavailable = styled.Text`
+  font-family: ${(props) => props.theme.fonts.monospace};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+  color: ${(props) => props.theme.colors.ui.error};
+`;
+
+const Category = styled.Image`
+  width: 15px;
+  height: 15px;
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://www.yourcoffeebreak.co.uk/wp-content/uploads/Sticks-n-Sushi-in-Islington-London-600x900.jpg",
     ],
     address = "Some random address",
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily,
+    isClosedTemporarily = true,
   } = restaurant;
+
+  const ratingArray = Array.from({ length: Math.floor(rating) });
 
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Unavailable>CLOSED TEMPORARILY</Unavailable>
+            )}
+            {isOpenNow && (
+              <Spacer position="left" size="large">
+                <SvgXml xml={open} width={20} height={20} />
+              </Spacer>
+            )}
+            <Spacer position="left" size="large">
+              <Category source={{ uri: icon }} />
+            </Spacer>
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
