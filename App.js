6dab722729c94334b4d5808fde63c2d22e38796c
OtakuMuch <nanimo1223@protonmail.com>
2025-02-06 13:12:02 +0800

    Added Tab Navigation

--- App.js
+++ App.js
@@ -1,14 +1,37 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import { useFonts } from "expo-font";
 import { Quicksand_400Regular } from "@expo-google-fonts/quicksand";
 import { BebasNeue_400Regular } from "@expo-google-fonts/bebas-neue";
 import { OverpassMono_400Regular } from "@expo-google-fonts/overpass-mono";
+import { Ionicons } from "@expo/vector-icons";
 
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { SettingsScreen } from "./src/features/settings/screens/settings.screen";
+import { MapScreen } from "./src/features/map/screens/map.screen";
 import { theme } from "./src/infrastructure/theme/index";
+import { restaurantsRequest } from "./src/services/restaurants/restaurants.service";
+
+const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Restaurants: "restaurant",
+  Map: "map",
+  Settings: "settings",
+};
+
+const screenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+  };
+};
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -24,9 +47,21 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
-        <StatusBar style="auto" />
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={screenOptions}
+            tabBarOptions={{
+              activeTintColor: "tomato",
+              inactiveTintColor: "gray",
+            }}
+          >
+            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+            <Tab.Screen name="Map" component={MapScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
+      <StatusBar style="auto" />
     </>
   );
 }
