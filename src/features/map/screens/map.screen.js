49db2555e09cb5ded3bd02f6e2381ba291b20a03
OtakuMuch <nanimo1223@protonmail.com>
2025-02-09 14:45:45 +0800

    Fixed Template to make use of SafeArea Utility

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,18 +1,11 @@
 import React from "react";
 import { Text } from "react-native";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import styled from "styled-components/native";
-
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-`;
+import { SafeArea } from "../../../components/utility/safe-area.component";
 
 export const MapScreen = () => {
   return (
-    <SafeAreaProvider>
-      <SafeArea>
-        <Text>Map Screen</Text>
-      </SafeArea>
-    </SafeAreaProvider>
+    <SafeArea>
+      <Text>Map Screen</Text>
+    </SafeArea>
   );
 };
