cd8811802397be5bac71cabce62ffa63bc815794
OtakuMuch <nanimo1223@protonmail.com>
2025-02-10 23:34:00 +0800

    feat: add compact restaurant info and search functionality to map screen

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,11 +1,67 @@
-import React from "react";
-import { Text } from "react-native";
-import { SafeArea } from "../../../components/utility/safe-area.component";
+import React, { useContext, useState, useEffect, useRef } from "react";
+import MapView, { Marker, Callout } from "react-native-maps";
+import styled from "styled-components/native";
 
-export const MapScreen = () => {
+import { Search } from "../components/search.component";
+import { LocationContext } from "../../../services/location/location.context";
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+import { MapCallout } from "../components/map-callout.component";
+
+const Map = styled(MapView)`
+  height: 100%;
+`;
+
+export const MapScreen = ({ navigation }) => {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
+
+  const mapRef = useRef(null);
+  const [region, setRegion] = useState(null);
+
+  useEffect(() => {
+    const { lat, lng, viewport } = location;
+    const latDelta = viewport.northeast.lat - viewport.southwest.lat;
+
+    const newRegion = {
+      latitude: lat,
+      longitude: lng,
+      latitudeDelta: latDelta,
+      longitudeDelta: 0.02,
+    };
+
+    setRegion(newRegion);
+    if (mapRef.current) {
+      mapRef.current.animateToRegion(newRegion, 1000);
+    }
+  }, [location]);
   return (
-    <SafeArea>
-      <Text>Map Screen</Text>
-    </SafeArea>
+    <>
+      <Search />
+      {region && (
+        <Map ref={mapRef} region={region}>
+          {restaurants.map((restaurant) => (
+            <Marker
+              key={restaurant.name}
+              title={restaurant.name}
+              coordinate={{
+                latitude: restaurant.geometry.location.lat,
+                longitude: restaurant.geometry.location.lng,
+              }}
+            >
+              <Callout
+                onPress={() =>
+                  navigation.navigate("Restaurants", {
+                    screen: "RestaurantsDetail",
+                    params: { restaurant },
+                  })
+                }
+              >
+                <MapCallout restaurant={restaurant} />
+              </Callout>
+            </Marker>
+          ))}
+        </Map>
+      )}
+    </>
   );
 };
